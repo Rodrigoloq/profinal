396f136771940181a38f3e527e1b3ecab373b175
Rodrigoloq <rodcxd1234@gmail.com>
2023-09-11 18:49:10 -0500

    api login v2

---
+++ inicio_sesion.php
@@ -0,0 +1,22 @@
+<?php
+    require_once("conexion.php");
+    $usuario = $_POST["usuario"];
+    $clave = $_POST["clave"];
+
+    $resultado = $conexion->query("SELECT * FROM Tb_Usuario WHERE Login_usuario = $usuario");
+
+    if ($resultado->num_rows == 1) {
+        $fila = $resultado->fetch_assoc();
+        if ($fila["Pass_Usuario"] == $clave) {
+            $registro[] = $fila;
+            echo json_encode($registro, JSON_UNESCAPED_UNICODE);
+        }else{
+            echo "-1";
+        }
+        
+    } else {
+        echo "-2";
+    }
+    //cerrar la conexion a la base de datos
+    $conexion->close(); 
+?>
