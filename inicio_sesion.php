bb56e36000cf33b3a8cd5f7b4b855924664a2aaa
Rodrigoloq <rodcxd1234@gmail.com>
2023-09-11 19:01:16 -0500

    api login v6

--- inicio_sesion.php
+++ inicio_sesion.php
@@ -6,11 +6,11 @@
     $usuario = $_POST["usuario"];
     $clave = $_POST["clave"];
     $rs = mysqli_query(	$cn,
-        "select * from clientes 
-    where usuario='" . $usuario . "'");
+        "select * from Tb_Usuario 
+    where Login_usuario='" . $usuario . "'");
     if (mysqli_num_rows($rs) == 1) {
         $row = mysqli_fetch_assoc($rs);
-        if ($row["clave"] == $clave) {
+        if ($row["Pass_Usuario"] == $clave) {
             $res[] = array_map("utf8_encode", $row);
             echo json_encode($res);
         } else {
