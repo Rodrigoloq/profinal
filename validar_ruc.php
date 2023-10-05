4b5184ce4b2fd5e4be1bc6d9f9364f27f045bf64
Rodrigoloq <rodcxd1234@gmail.com>
2023-10-05 16:12:19 -0500

    api prod y prov v8

--- validar_ruc.php
+++ validar_ruc.php
@@ -1,24 +1,23 @@
-<?php
-    include("conexion.php");
+<?php 
 
-    $cn = fnconexion();
+include("conexion.php");
 
-    $ruc = $_POST["vruc"] ?? null;
+$conexion = fnconexion();
+
+$ruc = $_POST["vruc"] ?? null;
+
+$sql = "select * from Tb_Proveedores
+where RUC='" . $ruc . "'";
+
+$resultado = $conexion->query($sql);
+
+if ($resultado->num_rows > 0) {
     
-    $rs = mysqli_query(	$cn,
-        "select * from Tb_Proveedores 
-    where RUC='" . $ruc . "'");
-    if (mysqli_num_rows($rs) == 1) {
-        $row = mysqli_fetch_assoc($rs);
-        if ($row["Pass_Usuario"] == $clave) {
-            $res[] = array_map(NULL, $row);
-            echo json_encode($res);
-        } else {
-            echo "-2";
-        }
-    } else {
-        echo "-1";
-    }
-    mysqli_close($cn);
-    //cerrar la conexion a la base de datos
+    echo "-1";
+} else {
+    echo "-2";
+}
+
+// Cerrar la conexión a la base de datos
+$conexion->close();
 ?>
