c6f5afb33195ab4fcfb42f7dd0245688c5824e6e
Rodrigoloq <rodcxd1234@gmail.com>
2023-10-16 11:48:44 -0500

    api combos orden v1

---
+++ listar_empleados.php
@@ -0,0 +1,31 @@
+<?php 
+
+include("conexion.php");
+
+$conexion = fnconexion();
+
+$sql = 'call Sp_Listar_Empleados()';
+
+$resultado = $conexion->query($sql);
+
+if ($resultado->num_rows > 0) {
+    // Crear un array para almacenar los resultados
+    $registros = array();
+
+    while ($fila = $resultado->fetch_assoc()) {
+        // Agregar cada fila al array de resultados
+        $registros[] = $fila;
+    }
+
+    // Convertir el array de resultados a formato JSON
+    $json_resultados = json_encode($registros);
+
+    // Mostrar el JSON
+    echo $json_resultados;
+} else {
+    echo 'No se encontraron registros.';
+}
+
+// Cerrar la conexión a la base de datos
+$conexion->close();
+?>
