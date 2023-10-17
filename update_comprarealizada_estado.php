dd325ae2b37807e4dab897fb72e621f81dda1032
Rodrigoloq <rodcxd1234@gmail.com>
2023-10-17 12:39:12 -0500

    api compra y orden v3

--- update_comprarealizada_estado.php
+++ update_comprarealizada_estado.php
@@ -9,21 +9,17 @@ $cn = fnconexion();
 
 $idcompra = $_POST["idcompra"];
 
-$idempleado = $_POST["idempleado"];
-
-$fecha = $_POST["fecha"];
-
 $estado = $_POST["estado_ingreso"];
 
 $usuario = $_POST["Usu_registro"];
 
 //agregar una sentencia sql para insertar datos
 
-$sql = "CALL Sp_Insert_CompraconDetalle(?, ?, ?, ?, ?)";
+$sql = "CALL Sp_Update_CompraRealizada_Estado(?, ?, ?)";
 
 $stmt = $cn->prepare($sql);
 
-$stmt->bind_param("iisis", $idcompra, $idempleado, $fecha, $estado, $usuario);
+$stmt->bind_param("iis", $idcompra, $estado, $usuario);
 
 if ($stmt->execute()) {
     echo "-1";
