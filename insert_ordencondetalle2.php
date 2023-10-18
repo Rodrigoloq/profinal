4d1be7fd3e35d9d51ffae185b109208e68a99218
Rodrigoloq <rodcxd1234@gmail.com>
2023-10-17 20:23:36 -0500

    api compra y orden v7

---
+++ insert_ordencondetalle2.php
@@ -0,0 +1,37 @@
+<?php
+include("conexion.php");
+$cn = fnconexion();
+
+$idempleado = $_POST["idempleado"];
+
+$fecha = $_POST["fecha"];
+
+$usuario = $_POST["Usu_registro"];
+
+$cn->begin_transaction();
+
+$sql = "INSERT INTO Tb_Orden (IdEmpleado, FechaOrden) VALUES (?, ?)";
+$stmt = $cn->prepare($sql);
+$stmt->bind_param("is", $idempleado, $fecha);
+$stmt->execute();
+$stmt->close();
+
+$idOrden = $cn->insert_id;
+
+$sql = "CALL Sp_InsertarDetalleOrden(?,?,?,?)";
+$stmt = $cn->prepare($sql);
+
+foreach ($_POST['detalles'] as $detalles) {
+    $idProducto = $detalles['id_producto'];
+    $cantidad = $detalles['cantidad'];
+
+    $stmt->bind_param("iiis", $idOrden, $idProducto, $cantidad, $usuario);
+    $stmt->execute();
+}
+
+$stmt->close();
+
+$cn->commit();
+
+$cn->close();
+?>
