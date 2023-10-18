8d95384e47489a0fb36b479f14352f02a4196a04
Rodrigoloq <rodcxd1234@gmail.com>
2023-10-17 21:45:29 -0500

    api compra y orden v12

--- insert_ordencondetalle2.php
+++ insert_ordencondetalle2.php
@@ -12,34 +12,50 @@ $detalles_json = $_POST['detalles'];
 
 $detalles = json_decode($detalles_json, true);
 
-$cn->begin_transaction();
+$mensaje = '';
 
+$conn->begin_transaction();
+
+// 1. Inserta un registro en la tabla "Compra"
 $sql = "INSERT INTO Tb_Orden (IdEmpleado, FechaOrden) VALUES (?, ?)";
 $stmt = $cn->prepare($sql);
 $stmt->bind_param("is", $idempleado, $fecha);
-$stmt->execute();
-$stmt->close();
 
-$idOrden = $cn->insert_id;
+if ($stmt->execute()) {
+    // Éxito en la inserción de Compra
+    $idOrden = $cn->insert_id;
 
-$sql = "CALL Sp_InsertarDetalleOrden(?,?,?,?)";
-$stmt = $cn->prepare($sql);
+    // 2. Inserta registros en la tabla "Detalle_Compra"
+    $sql = "CALL Sp_InsertarDetalleOrden(?,?,?,?)";
+    $stmt = $cn->prepare($sql);
 
-foreach ($detalles as $detalle) {
-    $idProducto = $detalle['id_producto'];
-    $cantidad = $detalle['cantidad'];
+    // Supongamos que tienes un bucle para procesar varios productos
+    foreach ($detalles as $detalle) {
+        $idProducto = $detalle['id_producto'];
+        $cantidad = $detalle['cantidad'];
 
-    $stmt->bind_param("iiis", $idOrden, $idProducto, $cantidad, $usuario);
-    if ($stmt->execute()) {
-        echo "-1";
-    } else {
-        echo "-2";
+        $stmt->bind_param("iiis", $idOrden, $idProducto, $cantidad, $usuario);
+
+        if (!$stmt->execute()) {
+            // Error en la inserción de Detalle_Compra
+            $mensaje = "-2";
+            break; // Puedes detener el bucle si hay un error
+        }
     }
+
+    if (empty($mensaje)) {
+        // Si llegas a este punto sin errores, confirma la transacción
+        $cn->commit();
+        $mensaje = "-1";
+    }
+} else {
+    // Error en la inserción de Compra
+    $mensaje = "-2";
 }
 
-$stmt->close();
-
-$cn->commit();
-
+// Cierra la conexión a la base de datos
 $cn->close();
+
+// Mostrar el mensaje de éxito o error
+echo $mensaje;
 ?>
